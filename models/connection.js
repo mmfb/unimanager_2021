ce0477de8de0cd8a89e1318fc41df7ddda7d5b23
Miguel Bugalho <miguel.bugalho@gmail.com>
2021-10-31 18:31:09 +0000

    Changed database connection string for online database

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-const connectionString = "postgres://postgres:root@localhost:5432/unimanager"
+const connectionString = "postgres://htfncprrfygtdc:71af38061a0904633fef4ffec874644e35ef40719081e39f4f27507554e601a4@ec2-52-206-193-199.compute-1.amazonaws.com:5432/dajf1d0bm2brh0"
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
